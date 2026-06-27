d01316a3d31ea41d85050e5e1156962041830a54
Adrineitor2 <adrian_fco1@hotmail.com>
2026-06-27 10:52:43 -0600

    cambio

---
+++ JavaScript/barra_de_nav.js
@@ -0,0 +1,8 @@
+//generar un nuevo enlace
+const nuevoEnlace = document.createElement('A');
+//agregar el href
+nuevoEnlace.href ='index.html';
+//agregar el texto
+nuevoEnlace.textContent='inicio';
+//agregar la clase
+nuevoEnlace.classList.add('navini contenedor');
