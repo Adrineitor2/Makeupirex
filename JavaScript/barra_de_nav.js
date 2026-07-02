a71fa660195795e031e893ee00992ad8874bb274
Adrineitor2 <adrian_fco1@hotmail.com>
2026-07-02 13:39:52 -0600

    Update barra_de_nav.js

--- JavaScript/barra_de_nav.js
+++ JavaScript/barra_de_nav.js
@@ -1,8 +1,18 @@
 //generar un nuevo enlace
-const nuevoEnlace = document.createElement('A');
+const nuevoEnlace = document.createElement('a');
 //agregar el href
 nuevoEnlace.href ='index.html';
 //agregar el texto
-nuevoEnlace.textContent='inicio';
+nuevoEnlace.textContent='inici';
 //agregar la clase
-nuevoEnlace.classList.add('navini');
+nuevoEnlace.classList.add('nav-bg');
+//agregar al documento
+const navegacion = document.querySelector('.navini');
+navegacion.appendChild(nuevoEnlace);
+console.log(nuevoEnlace);
+
+//seleccionar elementos y asociarles un evento
+const btnEnviar = document.querySelector('.boton');
+btnEnviar.addEventListener('click',function(){
+console.log('enviando formulario')
+});
